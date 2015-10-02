78a33ce91d029ba63b897e324e023ff22d766403
Brice <bgatelet@gmail.com>
2015-10-02 18:35:53 +0200

    Initial Commit

---
+++ Flag-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flag-App
+//
+//  Created by Brice Gatelet on 10/2/15.
+//  Copyright © 2015 Brice Gatelet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
